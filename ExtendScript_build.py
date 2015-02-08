912948f4ac3f962b673a703840975b3d3db5a4d8
runegan <runegan@gmail.com>
2015-02-08 02:14:42 +0100

    Loading Settings and user settings is working

--- ExtendScript_build.py
+++ ExtendScript_build.py
@@ -1,7 +1,49 @@
+import os
 import sys
 import subprocess
+import json
+from pprint import pprint
 
-script, scriptFile, packages = sys.argv
+script, file_handle, packages = sys.argv
 
-subprocess.call("arch -x86_64 osascript '%s/ExtendScript/run.scpt' '%s'" %(packages, scriptFile), shell=True)
-subprocess.call("echo done", shell=True);
+buildFolder = packages+'/ExtendScript'
+
+
+# Get default settings
+with open(buildFolder+'/ExtendScript.sublime-settings') as settingsFile:
+	settings = json.load(settingsFile)
+
+user_settings = ""
+user_settingsFile = packages+'/User/ExtendScript.sublime-settings'
+
+# Check if user settings file exists and is not empty
+if os.path.exists(user_settingsFile):
+	if os.stat(user_settingsFile).st_size != 0:
+		with open(user_settingsFile) as settingsFile:
+			user_settings = json.load(settingsFile)
+
+		# Overwrite default settings with the user settings
+		for setting in settings:
+			if setting in user_settings:
+				settings[setting] = user_settings[setting]
+
+
+if not os.path.exists(file_handle):
+	print 'The file must be saved first to be built'
+
+else:
+	with open(file_handle, 'r') as file_handle:
+		file_string = file_handle.read().decode("utf-8-sig")
+		file_handle.close()
+
+		file_output = ""
+
+
+		appleScriptFile = packages+'/ExtendScript/run.scpt'
+
+		subprocess.call('arch -x86_64 '
+			'osascript "'+appleScriptFile+'" "'+file_handle+'"',
+			shell=True)
+
+
+		print 'done'

c290a247ad14663abd0a23113d369c62c39c0165
runegan <runegan@gmail.com>
2015-02-09 20:56:04 +0100

    Working on build system, #target

--- ExtendScript_build.py
+++ ExtendScript_build.py
@@ -10,48 +10,56 @@ buildFolder = packages+'/ExtendScript'
 
 # Get default settings
 with open(buildFolder+'/ExtendScript.sublime-settings') as settingsFile:
-	settings = json.load(settingsFile)
-
-user_settings = ""
-user_settingsFile = packages+'/User/ExtendScript.sublime-settings'
-
-# Check if user settings file exists and is not empty
-if os.path.exists(user_settingsFile):
-	if os.stat(user_settingsFile).st_size != 0:
-		with open(user_settingsFile) as settingsFile:
-			user_settings = json.load(settingsFile)
-
-		# Overwrite default settings with the user settings
-		for setting in settings:
-			if setting in user_settings:
-				settings[setting] = user_settings[setting]
+    settings = json.load(settingsFile)
 
 
 file_path = currentPath+'/'+currentFileName
 
-
 if not os.path.exists(file_path):
-	print 'The file must be saved first to be built'
+    print 'The file must be saved first to be built'
 
-else:	
-	with open(file_path, 'r') as file_handleFile:
-		file_string = file_handleFile.read().decode("utf-8-sig")
-		file_handleFile.close()
+else:
+    with open(file_path, 'r') as file_handleFile:
+        file_string = file_handleFile.read().decode("utf-8-sig")
 
-		
-		file_output = ""
+        targetApp = settings['default_app']
 
-		for line in file_string.splitlines():
-			if re.match(r'.*#target', line):
-				targetApp = re.sub(r'.*#target \"(.*)\";?', r'\1', line)
-				print "Target app is: " + targetApp
-				break
+        for line in file_string.splitlines():
+            if re.match(r'.*#target', line):
+                regex = r'.*#target [\"\']?([^\"\']*)[\"\']?;?'
+                targetApp = re.sub(regex, r'\1', line)
+                break
 
-		# appleScript_path = packages+'/ExtendScript/run.scpt'
+        if targetApp == "":
+            print "Error: No target app"
 
-		# subprocess.call('arch -x86_64 '
-		# 	'osascript "'+appleScript_path+'" "'+file_path+'"',
-		# 	shell=True)
+        else:
 
+            appAE = ['after effects', 'after-effects', 'aftereffects', 'ae']
+            appPS = ['photoshop', 'ps']
+            appAI = ['illustrator', 'ai']
+            appID = ['indesign', 'id']
 
-		print 'Done'
+            targetApp = targetApp.lower()
+
+            if targetApp in appAE:
+                targetApp = 'After Effects'
+
+            elif targetApp in appPS:
+                targetApp = 'Photoshop'
+
+            elif targetApp in appAI:
+                targetApp = 'Illustrator'
+
+            elif targetApp in appID:
+                targetApp = 'InDesign'
+
+            print "Target app is: " + targetApp
+
+            # appleScript_path = packages+'/ExtendScript/run.scpt'
+
+            # subprocess.call('arch -x86_64 '
+            #   'osascript "'+appleScript_path+'" "'+file_path+'"',
+            #   shell=True)
+
+            print 'Done'
